99f6bcb2eedad16058056bca9e23d194dd5bb574
gen8-ai <genr8.ai@gmail.com>
2025-04-22 14:48:00 +1000

    Update repository link in main.js and add script reference in 404.html; remove home.html

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // This file is part of the "HTML5" project.
 
 document.getElementById("ctaRepository").addEventListener("click", function goToGit() {
-    const url = "https://github.com";
+    const url = "https://github.com/Gen8-ai/HTML5-boilerplate-by-lachlan-walshe";
     window.open(url, "_blank");
     window.focus();
     return false;
-}),
+});
 
 document.getElementById("ctaDownload").addEventListener("click", function getProjectZip() {
     const url = "https://github.com/Gen8-ai/HTML5-boilerplate-by-lachlan-walshe/archive/refs/heads/main.zip";
